26375d074a83085e39ab8e231ea893dc6d8e0976
Marcus Ziadé <marcus.ziade@ouraring.com>
2022-10-10 21:14:40 +0300

    Initial Commit

---
+++ Spongercaser/SpongercaserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpongercaserApp.swift
+//  Spongercaser
+//
+//  Created by Marcus Ziadé on 10.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpongercaserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
